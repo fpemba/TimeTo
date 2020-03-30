714817f7b29859149387284528f2da56038363ce
Gaelan Shively <gaelanshively@gmail.com>
2020-03-30 14:46:25 -0400

    created backend with Spring Initializr

---
+++ backend-SPA/src/test/java/com/TimeTo/TimeTo/TimeToApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TimeTo.TimeTo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeToApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
