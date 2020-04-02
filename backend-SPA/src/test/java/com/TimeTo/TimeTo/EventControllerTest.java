861ecd4705af802208113b497cc0275349641910
Anna Lizon <annakatarina1994@gmail.com>
2020-04-02 13:55:12 -0400

    Working on getting test to retrieve event by id to pass

---
+++ backend-SPA/src/test/java/com/TimeTo/TimeTo/EventControllerTest.java
@@ -0,0 +1,40 @@
+package com.TimeTo.TimeTo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class EventControllerTest {
+    private MockMvc mockMvc;
+    private EventController eventController;
+    private EventRepository eventRepo;
+    private DayRepository dayRepo;
+
+    @BeforeEach
+    public void setUp(){
+        eventRepo = mock(EventRepository.class);
+        dayRepo = mock(DayRepository.class);
+        eventController = new EventController(eventRepo);
+        mockMvc = MockMvcBuilders.standaloneSetup(eventController).build();
+    }
+
+    @Test
+    public void shouldRetrieveEvents() throws Exception{
+        Day testDay = new Day();
+    }
+
+    @Test
+    public void shouldRetrieveEventById() throws Exception{
+        Day testDay = new Day();
+        Event testEvent = new Event(testDay);
+        when(eventRepo.findById(testEvent.getId())).thenReturn(java.util.Optional.of(testEvent));
+        mockMvc.perform(MockMvcRequestBuilders.get("/events/1"))
+                .andExpect(status().isOk());
+    }
+}
