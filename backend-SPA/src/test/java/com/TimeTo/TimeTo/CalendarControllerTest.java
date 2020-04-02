78cb82cefee6d4c1ddf67d2991f0e69fa4565e35
fpemba <fatuma.pemba@yahoo.com>
2020-04-02 13:54:10 -0400

    Started HttpRequest tests, currently not passning

--- backend-SPA/src/test/java/com/TimeTo/TimeTo/CalendarControllerTest.java
+++ backend-SPA/src/test/java/com/TimeTo/TimeTo/CalendarControllerTest.java
@@ -2,6 +2,7 @@ package com.TimeTo.TimeTo;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -13,6 +14,7 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@DirtiesContext
 public class CalendarControllerTest {
    private CalendarRepository calendarRepository;
    private CalendarController underTest;
--- backend-SPA/src/test/java/com/TimeTo/TimeTo/TimeToApplicationTests.java
+++ backend-SPA/src/test/java/com/TimeTo/TimeTo/TimeToApplicationTests.java
@@ -2,8 +2,10 @@ package com.TimeTo.TimeTo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
+@DirtiesContext
 class TimeToApplicationTests {
 
 	@Test
