0e28592a9a3cfd240d4b97fe8bf3f914e8c1e977
Anna Lizon <annakatarina1994@gmail.com>
2020-04-03 10:36:11 -0400

    homePageCreator now contains constructor and render()

---
+++ frontend-SPA/src/js/homePageCreator.js
@@ -0,0 +1,9 @@
+class HomePageCreator{
+
+    constructor(){
+    this._main = document.createElement("main");
+    this._header = document.createElement("header");
+    this._footer = document.createElement("footer");
+    
+    }
+}
